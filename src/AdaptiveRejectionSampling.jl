886a22e75ef8f10c89beafa29c002c1e1032cf64
mauriciogtec <mauriciotec@gmail.com>
2018-02-02 02:24:58 -0600

    sdf

--- src/AdaptiveRejectionSampling.jl
+++ src/AdaptiveRejectionSampling.jl
@@ -1,94 +1,101 @@
 module AdaptiveRejectionSampling
 using ForwardDiff
 # ------------------------------
-import Base.indices
-
-"""
-A structure for LineSegment
-"""
-mutable struct LineSegment
-    slope::AbstractFloat
-    intercept::AbstractFloat
-    lower::AbstractFloat
-    upper::AbstractFloat
-    LineSegment(m, b, lower, upper) =
-        (lower < upper) ?
-            new(m, b, lower, upper) : throw(ArgumentError("lower !< upper"))
-end
 
 """
 Line
 """
 mutable struct Line
-    slope::AbstractFloat
-    intercept::AbstractFloat
+    slope::Float64
+    intercept::Float64
+end
+
+"""
+Find the intersection between lines
+"""
+function intersection(l1::Line, l2::Line)
+    @assert l1.slope != l2.slope "slopes should be different"
+    x = - (l2.intercept - l1.intercept) / (l2.slope - l1.slope)
+    # y = l1.slope * x + l1.intercept
+    x
 end
 
 """
 Ordered Partition
 """
 mutable struct OrderedPartition
-    lines::Array{Line, 1}
-    cuts::Array{AbstractFloat, 1}
-    size::Integer
-    OrderedPartition(lines::Array{Line, 1}, cuts::Array{AbstractFloat, 1}) =
-        issorted(cuts) && length(unique(cuts)) == length(cuts) ?
-            new(lines, cuts) : throw(ArgumentError("cutpoints must be sorted"))
-end
-
-"""
-Find the intersection between lines
-"""
-function intersection(l1::Union{LineSegment, Line}, l2::Union{LineSegment, Line})
-    if l1.slope == l2.slope
-        throw(ArgumentError("lines don't intersect, slopes must differ"))
-    else
-        x = - (l2.intercept - l1.intercept) / (l2.slope - l1.slope)
-        y = l1.slope * x + l1.intercept
-        return x, y
+    lines::Vector{Line}
+    cutpoints::Float64
+    size::Int32
+    weights::Float64
+    OrderedPartition(lines::Vector{Line}) = begin
+        @assert issorted([l.slope for l in lines], rev = true) "line slopes must be decreasing"
+        cutpoints = [intersection(lines[i], lines[i + 1]) for i in 1:(length(lines) - 1)]
+        @assert issorted(cutpoints) "resultin cutpoints aren't ordered"
+        @assert length(unique(cutpoints)) == length(cutpoints) "cutpoints can't contain duplicates"
+        weights = [exp(l.slope[i])]
+        new(lines, cutpoints, length(lines), weights)
     end
 end
 
+
+"""
+Adds a new line segment to an ordered partition based on its slope
+"""
+function add_line_segment!(p::OrderedPartition, l::Line)
+    # Find the position
+    pos = 1
+    while l.slope < p.lines[pos].slope || pos > length(p.lines)
+        pos += 1
+    end
+
+    # Insert line segment
+    if pos == 1 || pos == length(p.lines) + 1
+        cut = intersection(l, p.lines[pos])
+        insert!(p.cutpoints, pos, cut)
+    else
+        cut1 = intersection(l, p.lines[pos - 1])
+        cut2 = intersection(l, p.lines[pos])
+        splice!(p.cutpoints, pos - 1, [cut1, cut2])
+        @assert issorted(p.cutpoints)  "resulting intersection points aren't sorted"
+    end
+    insert!(p.lines, pos, l)
+end
+
+
+# --------------------------------
+export Line, OrderedPartition
+export intersection, add_line_segment!
+# --------------------------------
 # """
-# Add a new line to a LineSegment array
+# A Majorizer most contain information about the problem
 # """
-# function add_line_segment!(arr::Array{LineSegment}, newl::LineSegment)
-#
+# mutable struct Envelop
+#     line_segments::Array{LineSegment, 1}
+#     logfun::Function
+#     loggrad::Function
+#     loghess::Function
 # end
-
-# --------------------------------
-# export LineSegment, slope, intercept, intersection
-export LineSegment, intersection
-# --------------------------------
-"""
-A Majorizer most contain information about the problem
-"""
-mutable struct Envelop
-    line_segments::Array{LineSegment, 1}
-    logfun::Function
-    loggrad::Function
-    loghess::Function
-end
-
-"""
-Eval gradient of Envelop
-"""
-function initialize_envelop(fun::Function, x1::AbstractFloat, x2::AbstractFloat)
-    @assert x1 < x2
-    logfun(x) = log(fun(x))
-    loggrad(x) = ForwardDiff.derivative(logfun, x)
-    @assert loggrad(x1) > 0
-    @assert loggrad(x2) < 0
-    loghess(x) = ForwardDiff.derivative(loggrad, x)
-    m1, m2 = loggrad(x1), loggrad(x2)
-    b1, b2 = logfun(x2), logfun(x2)
-    l1, l2 = LineSegment(m1, b1, -Inf, Inf), LineSegment(m2, b2, -Inf, Inf)
-    intersectx, intersecty = intersection(l1, l2)
-    l1.upper = intersectx
-    l2.lower = intersectx
-    return Envelop([l1, l2], logfun, loggrad, loghess)
-end
-
-# ------------------------------------
-export Envelop, initialize_envelop
+#
+# """
+# Eval gradient of Envelop
+# """
+# function initialize_envelop(fun::Function, x1::AbstractFloat, x2::AbstractFloat)
+#     @assert x1 < x2
+#     logfun(x) = log(fun(x))
+#     loggrad(x) = ForwardDiff.derivative(logfun, x)
+#     @assert loggrad(x1) > 0
+#     @assert loggrad(x2) < 0
+#     loghess(x) = ForwardDiff.derivative(loggrad, x)
+#     m1, m2 = loggrad(x1), loggrad(x2)
+#     b1, b2 = logfun(x2), logfun(x2)
+#     l1, l2 = LineSegment(m1, b1, -Inf, Inf), LineSegment(m2, b2, -Inf, Inf)
+#     intersectx, intersecty = intersection(l1, l2)
+#     l1.upper = intersectx
+#     l2.lower = intersectx
+#     return Envelop([l1, l2], logfun, loggrad, loghess)
+# end
+#
+# # ------------------------------------
+# export Envelop, initialize_envelop
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,21 +1,37 @@
-import AdaptiveRejectionSampling
-m = AdaptiveRejectionSampling
+using AdaptiveRejectionSampling
 
 using Base.Test
 
-# # write your own tests here
-# @testset "Lines, slopes and interceptions" begin
-#     # @test m.LineSegment(1, 2, -1.0, 3) isa m.LineSegment
-#     # @test_throws ArgumentError m.LineSegment(0, 0, 0.0 ,0)
-#     # @test m.slope(m.LineSegment(0, 0, 1, 1)) == 1.0
-#     # @test m.slope(m.LineSegment(0, 0, 0, 1)) == Inf
-#     # @test m.intercept(m.LineSegment(0, 1, 1, 0)) == 1.0
-# end
+@testset "Lines and intersection" begin
+    @test Line(2.0, 3) isa Line
+    @test intersection(Line(-1.0, 1.0), Line(1.0, -1.0)) == 1.0
+    @test_throws AssertionError intersection(Line(-1.0, 1.0), Line(-1.0, -1.0))
+end
 
-# write your own tests here
-@testset "Lines and intersections" begin
-    @test m.LineSegment(-1.0, 4.0, -Inf, 10.0) isa m.LineSegment
-    @test_throws ArgumentError m.LineSegment(-1.0, 4.0, Inf, 10.0)
-    @test m.intersection(m.LineSegment(1.0, .0, -Inf, Inf), m.LineSegment(-1.0, 1.0, -Inf, Inf)) == (0.5, 0.5)
-    @test_throws ArgumentError m.intersection(m.LineSegment(0.0, 0.0, -Inf, Inf), m.LineSegment(0.0, 1.0, -Inf, Inf))
+@testset "Ordered partitions and add segments" begin
+    @test begin
+        l1 = Line(1.0, 1.0)
+        l2 = Line(-3.0, 2.0)
+        OrderedPartition([l1, l2])
+    end isa OrderedPartition
+    @test_throws AssertionError begin
+        l1 = Line(1.0, 1.0)
+        l2 = Line(-3.0, 2.0)
+        OrderedPartition([l2, l1])
+    end
+    @test begin
+        l1 = Line(1.0, 1.0)
+        l2 = Line(-3.0, 2.0)
+        l3 = Line(-5.0, 5.0)
+        p = OrderedPartition([l1, l2, l3])
+        p.cutpoints == [0.25, 1.5]
+    end
+    @test begin
+        l1 = Line(1.0, 1.0)
+        newline = Line(0.0, 0.0)
+        l3 = Line(-1.0, 1.0)
+        p = OrderedPartition([l1, l3])
+        add_line_segment!(p, newline)
+        p.lines[2] == newline
+    end
 end
