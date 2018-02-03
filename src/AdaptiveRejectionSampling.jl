2ae56fbbcce1cecde46e7e6877fa184d57848ade
mauriciogtec <mauriciotec@gmail.com>
2018-02-03 16:51:10 -0600

    v0.1

--- src/AdaptiveRejectionSampling.jl
+++ src/AdaptiveRejectionSampling.jl
@@ -1,8 +1,12 @@
 module AdaptiveRejectionSampling
+# ------------------------------
 using ForwardDiff
 using StatsBase
 # ------------------------------
+export Line, Objective, Envelop, RejectionSampler
+export run_sampler!, eval_envelop
 
+# ------------------------------
 """
 Line
 """
@@ -30,17 +34,17 @@ end
 """
 Ordered Partition
 """
-mutable struct OrderedPartition
+mutable struct Envelop
     lines::Vector{Line}
     cutpoints::Vector{Float64}
     weights::Vector{Float64}
-    OrderedPartition(lines::Vector{Line}) = begin
+    Envelop(lines::Vector{Line}) = begin
         @assert issorted([l.slope for l in lines], rev = true) "line slopes must be decreasing"
         cutpoints = [intersection(lines[i], lines[i + 1]) for i in 1:(length(lines) - 1)]
         @assert issorted(cutpoints) "resultin cutpoints aren't ordered"
         @assert length(unique(cutpoints)) == length(cutpoints) "cutpoints can't contain duplicates"
         int_lims = [-Inf; cutpoints; Inf]
-        weights = [exp_integral(lines[i], int_lims[i], int_lims[i + 1]) for i in 1:(length(int_lims) - 1)]
+        weights = [exp_integral(l, int_lims[i], int_lims[i + 1]) for (i, l) in enumerate(lines)]
         @assert Inf ∉ weights "Numerical error integrating: overflow"
         new(lines, cutpoints, weights)
     end
@@ -50,7 +54,7 @@ end
 """
 Adds a new line segment to an ordered partition based on its slope
 """
-function add_line_segment!(p::OrderedPartition, l::Line)
+function add_line_segment!(p::Envelop, l::Line)
     # Find the position in sorted array with binary search
     pos = searchsortedfirst([-line.slope for line in p.lines], -l.slope)
     # Insert line segment
@@ -68,11 +72,14 @@ function add_line_segment!(p::OrderedPartition, l::Line)
     end
     insert!(p.lines, pos, l)
     int_lims = [-Inf; p.cutpoints; Inf]
-    p.weights = [exp_integral(p.lines[i], int_lims[i], int_lims[i + 1]) for i in 1:(length(int_lims) - 1)]
-
+    p.weights = [exp_integral(line, int_lims[i], int_lims[i + 1]) for (i, line) in enumerate(p.lines)];
+    nothing
 end
 
-function get_samples(p::OrderedPartition, n::Int64)
+"""
+Sample from an envelop
+"""
+function get_samples(p::Envelop, n::Int64)
     line_num = sample(1:length(p.lines), weights(p.weights), n)
     a = [l.slope for l in p.lines]
     b = [l.intercept for l in p.lines]
@@ -84,15 +91,12 @@ end
 """
 Eval point on ordered partition
 """
-function exp_eval_point(p::OrderedPartition, x::Float64)
+function eval_envelop(p::Envelop, x::Float64)
     pos = searchsortedfirst(p.cutpoints, x)
     a, b = p.lines[pos].slope, p.lines[pos].intercept
     exp(a * x + b)
 end
 
-# --------------------------------
-export Line, OrderedPartition
-export intersection, add_line_segment!, exp_integral, get_samples, exp_eval_point
 # --------------------------------
 
 """
@@ -112,7 +116,7 @@ Rejection Sampler
 """
 mutable struct RejectionSampler
     objective::Objective
-    envelop::OrderedPartition
+    envelop::Envelop
     RejectionSampler(f::Function, x1::Float64, x2::Float64)  = begin #x1 and x2 should be auto
         logf(x) = log(f(x))
         objective = Objective(logf)
@@ -121,35 +125,48 @@ mutable struct RejectionSampler
         @assert a2 <= 0 "logf must have negative slope at x2"
         b1, b2 = objective.logf(x1) - a1 * x1, objective.logf(x2) - a2 * x2
         line1, line2 = Line(a1, b1), Line(a2, b2)
-        envelop = OrderedPartition([line1, line2])
+        envelop = Envelop([line1, line2])
         new(objective, envelop)
+    end
+    RejectionSampler(f::Function; δ = 0.5, max_attempts = 100)  = begin #x1 and x2 should be auto
+        logf(x) = log(f(x))
+        grad(x) = ForwardDiff.derivative(logf, x)
+        x1, x2 = -δ, δ
+        i, j = 0, 0
+        while grad(x1) <= 0 && i < max_attempts
+            x1 -= δ
+            i += 1
+        end
+        while grad(x2) >= 0 && j < max_attempts
+            x2 += δ
+            j += 1
+        end
+        @assert i != max_attempts && j != max_attempts "couldn't find initial points, please provide them or verify that f is logconcave"
+        RejectionSampler(f, x1, x2)
     end
 end
 
-function run_sampler!(sampler::RejectionSampler, n::Int)
+function run_sampler!(sampler::RejectionSampler, n::Int64; max_segments::Int64 = 25, max_failed_factor::Float64 = 0.01)
     i = 0
-    iter = 0
+    failed, max_failed = 0, trunc(Int, n / max_failed_factor)
     out = zeros(n)
-    acc_rate = 0.0
-    maxiter = 1e8
-    while i < n && iter < maxiter
+    while i < n
         candidate = get_samples(sampler.envelop, 1)[1]
-        acceptance_ratio = exp(sampler.objective.logf(candidate)) / exp_eval_point(sampler.envelop, candidate)
-        acc_rate += (1.0 / n) * acceptance_ratio
+        acceptance_ratio = exp(sampler.objective.logf(candidate)) / eval_envelop(sampler.envelop, candidate)
         if rand() < acceptance_ratio
             i += 1
             out[i] = candidate
         else
-            a = sampler.objective.grad(candidate)
-            b = sampler.objective.logf(candidate) - a * candidate
-            add_line_segment!(sampler.envelop, Line(a, b))
+            if length(sampler.envelop.lines) <= max_segments
+                a = sampler.objective.grad(candidate)
+                b = sampler.objective.logf(candidate) - a * candidate
+                add_line_segment!(sampler.envelop, Line(a, b))
+            end
+            failed += 1
+            @assert failed < max_failed "max_failed_factor reached"
         end
-        iter += 1
     end
-    println("Average acceptance rate:", acc_rate)
     out
 end
 
-# # ------------------------------------
-export RejectionSampler, Objective, Envelop, run_sampler!
-end # module
+end #
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,37 +1,70 @@
-using AdaptiveRejectionSampling
+import AdaptiveRejectionSampling
+arj = AdaptiveRejectionSampling
 
 using Base.Test
 
-@testset "Lines and intersection" begin
-    @test Line(2.0, 3) isa Line
-    @test intersection(Line(-1.0, 1.0), Line(1.0, -1.0)) == 1.0
-    @test_throws AssertionError intersection(Line(-1.0, 1.0), Line(-1.0, -1.0))
+@testset "Line" begin
+    @test arj.Line(2.0, 3) isa arj.Line
+    @test arj.intersection(arj.Line(-1.0, 1.0), arj.Line(1.0, -1.0)) == 1.0
+    @test_throws AssertionError arj.intersection(arj.Line(-1.0, 1.0), arj.Line(-1.0, -1.0))
 end
 
-@testset "Ordered partitions and add segments" begin
+@testset "Envelop" begin
     @test begin
-        l1 = Line(1.0, 1.0)
-        l2 = Line(-3.0, 2.0)
-        OrderedPartition([l1, l2])
-    end isa OrderedPartition
+        l1 = arj.Line(1.0, 1.0)
+        l2 = arj.Line(-3.0, 2.0)
+        arj.Envelop([l1, l2])
+    end isa arj.Envelop
     @test_throws AssertionError begin
-        l1 = Line(1.0, 1.0)
-        l2 = Line(-3.0, 2.0)
-        OrderedPartition([l2, l1])
+        l1 = arj.Line(1.0, 1.0)
+        l2 = arj.Line(-3.0, 2.0)
+        arj.Envelop([l2, l1])
     end
     @test begin
-        l1 = Line(1.0, 1.0)
-        l2 = Line(-3.0, 2.0)
-        l3 = Line(-5.0, 5.0)
-        p = OrderedPartition([l1, l2, l3])
+        l1 = arj.Line(1.0, 1.0)
+        l2 = arj.Line(-3.0, 2.0)
+        l3 = arj.Line(-5.0, 5.0)
+        p = arj.Envelop([l1, l2, l3])
         p.cutpoints == [0.25, 1.5]
     end
     @test begin
-        l1 = Line(1.0, 1.0)
-        newline = Line(0.0, 0.0)
-        l3 = Line(-1.0, 1.0)
-        p = OrderedPartition([l1, l3])
-        add_line_segment!(p, newline)
+        l1 = arj.Line(1.0, 1.0)
+        newline = arj.Line(0.0, 0.0)
+        l3 = arj.Line(-1.0, 1.0)
+        p = arj.Envelop([l1, l3])
+        arj.add_line_segment!(p, newline)
         p.lines[2] == newline
+    end
+end
+
+@testset "Objective" begin
+    @test arj.Objective(x -> exp(-0.5 * x^2) / sqrt(2pi)) isa arj.Objective
+    @test begin
+        objective = arj.Objective(x -> exp(-0.5 * x^2) / sqrt(2pi))
+        objective.logf(0.0) ≈ 0.3989422804014327
+    end
+    @test begin
+        objective = arj.Objective(x -> exp(-0.5 * x^2) / sqrt(2pi))
+        objective.grad(0.0) ≈ 0.0
+    end
+    @test begin
+        objective = arj.Objective(x -> exp(-0.5 * x^2) / sqrt(2pi))
+        objective.logf(Inf) == 0.0 && objective.logf(Inf) == 0.0
+    end
+end
+
+@testset "RejectionSampler" begin
+    @test arj.RejectionSampler(x -> exp(-0.5 * x^2) / sqrt(2pi), -1.0, 1.0) isa arj.RejectionSampler
+    @test begin
+        sampler = arj.RejectionSampler(x -> exp(-0.5 * x^2) / sqrt(2pi), -1.0, 1.0)
+        arj.run_sampler!(sampler, 5) isa Vector{T} where T <: AbstractFloat
+    end
+    @test begin
+        sampler = arj.RejectionSampler(x -> exp(-0.5 * x^2) / sqrt(2pi))
+        arj.run_sampler!(sampler, 5) isa Vector{T} where T <: AbstractFloat
+    end
+    @test_throws AssertionError begin
+        sampler = arj.RejectionSampler(x -> exp(-0.5 * x) / sqrt(2pi))
+        arj.run_sampler!(sampler, 5) isa Vector{T} where T <: AbstractFloat
     end
 end
